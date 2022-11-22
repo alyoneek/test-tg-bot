be1ca7cd160d916855d04dc92f4f547a692205b1
alyoneek <alyonta03@mail.ru>
2022-11-22 14:24:20 +0700

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import Header from './components/Header/Header';
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,18 +1,18 @@
-const tg = window.Telegram.WebApp;
-
-const onClose = () => {
-  tg.close();
-};
-
-const onToggle = () => {
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  } else {
-    tg.MainButton.show();
-  }
-};
-
 export function useTelegram() {
+  const tg = window.Telegram.WebApp;
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  const onToggle = () => {
+    if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  };
+
   return {
     tg,
     onClose,
